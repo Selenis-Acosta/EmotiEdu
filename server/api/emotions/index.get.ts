9adce3f15ed2b87f9ba82b239c192301cc7dc6d7
MarinellyPerez <143960647+MarinellyPerez@users.noreply.github.com>
2024-12-02 11:56:49 -0500

    ..

--- server/api/emotions/index.get.ts
+++ server/api/emotions/index.get.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event)=>{
     const emotions =[
         {
             name: "Euforia",
-            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png",
+            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635456_1280.png",
             content: {
               title: "Sentimientos de Euforia",
               videoUrl: '<iframe width="560" height="315" src="https://www.youtube.com/embed/p0fZXkmWGzE?si=QQ1dNG-ftcscgI_f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
@@ -12,7 +12,7 @@ export default defineEventHandler(async (event)=>{
           },
           {
             name: "Felicidad",
-            emojiUrl: "https://example.com/neutral.png",
+            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635451_1280.png",
             content: {
               title: "Sentimiento Feliz",
               videoUrl: "https://www.youtube.com/embed/neutral-video",
@@ -22,7 +22,7 @@ export default defineEventHandler(async (event)=>{
           },
           {
             name: "Tranquilidad",
-            emojiUrl: "https://example.com/neutral.png",
+            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635449_1280.png",
             content: {
               title: "Sentimiento Tranquilo",
               videoUrl: "https://www.youtube.com/embed/neutral-video",
@@ -32,7 +32,7 @@ export default defineEventHandler(async (event)=>{
           },
           {
             name: "Neutralidad",
-            emojiUrl: "https://example.com/neutral.png",
+            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635450_1280.png",
             content: {
               title: "Sentimiento Neutral",
               videoUrl: "https://www.youtube.com/embed/neutral-video",
@@ -42,7 +42,7 @@ export default defineEventHandler(async (event)=>{
           },
           {
             name: "Tristeza",
-            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635454_1280.png",
+            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/24/smiley-1635448_1280.png",
             content: {
               title: "Sentimiento de tristeza",
               videoUrl: "https://www.youtube.com/embed/neutral-video",
@@ -52,19 +52,9 @@ export default defineEventHandler(async (event)=>{
           },
           {
             name: "Frustración",
-            emojiUrl: "https://www.flaticon.es/icono-gratis/enojado_599633?related_id=599633&origin=pack",
+            emojiUrl: "https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635454_1280.png",
             content: {
               title: "Sentimiento de frustración",
-              videoUrl: "https://www.youtube.com/embed/neutral-video",
-              imageUrl: "https://example.com/neutral-image.jpg",
-              text: "Me encanta la energía tranquila que tienes hoy. Es contagiosa."
-            }
-          },
-          {
-            name: "Enojo",
-            emojiUrl: "https://example.com/neutral.png",
-            content: {
-              title: "Sentimiento de enojo",
               videoUrl: "https://www.youtube.com/embed/neutral-video",
               imageUrl: "https://example.com/neutral-image.jpg",
               text: "Me encanta la energía tranquila que tienes hoy. Es contagiosa."
