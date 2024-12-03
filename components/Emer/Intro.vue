2403afc4eb9b534ff5f4448bc457afdbf961849a
alesanz1 <143409115+alesanz1@users.noreply.github.com>
2024-12-02 21:03:24 -0500

    componentes

---
+++ components/Emer/Intro.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+     <v-container>
+    <!-- Título -->
+    <v-row class="text-center mb-5">
+      <v-col>
+        <v-card elevation="3" class="py-5">
+          <v-card-title class="text-h5">
+            Zona de Emergencia Emocional
+          </v-card-title>
+          <v-card-text>
+            Aquí puedes encontrar apoyo inmediato en situaciones críticas.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+     </v-container>
+    </div>
+  </template>
+  
+  
