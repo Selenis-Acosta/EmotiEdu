cfafd389888da7099ef56f82da30f5fb3fbcb915
alesanz1 <143409115+alesanz1@users.noreply.github.com>
2024-12-02 23:42:01 -0500

    componentes contacto

---
+++ components/Emergencia/contactos.vue
@@ -0,0 +1,9 @@
+
+        
+    <template> <v-row>
+      <v-col>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/FDr5iQYZDnQ?si=nOImNK0VZjhOSMNC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </v-col>
+      <v-col><iframe width="560" height="315" src="https://www.youtube.com/embed/im3vc2NzCVc?si=G-T-I_Fa7lzn0_-H" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></v-col>
+
+    </v-row></template>
