d4c244223465add0b8dd9112e4b5037263a83e96
Selenis_Acosta <143279342+Selenis-Acosta@users.noreply.github.com>
2024-12-02 22:57:06 -0500

    Cambios en el diseño de audios

--- components/Bienes/Audios.vue
+++ components/Bienes/Audios.vue
@@ -28,20 +28,20 @@
         podcasts: [
           {
             id: 1,
-            title: "Episodio 1: Manejo del estrés",
-            description: "Consejos para manejar el estrés diario.",
+            title: "Conflictos y Heridas Emocionales",
+           /* description: "Consejos para manejar el estrés diario.",*/
             audioUrl: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/5LaICDehMIlTIC1s0Tioh7?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
           },
           {
             id: 2,
-            title: "Episodio 2: Técnicas de relajación",
-            description: "Exploramos ejercicios para relajarte.",
+            title: "Episodio 2: Poner Limites",
+            /*description: "Exploramos ejercicios para relajarte.", */
             audioUrl: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/4y1laSf0GvCRPVFII2WO9q?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
           },
           {
             id: 3,
-            title: "Episodio 3: Hábitos saludables",
-            description: "Cómo construir una rutina mentalmente saludable.",
+            title: "Episodio 3: Acompañamiento Emocional",
+           /* description: "Cómo construir una rutina mentalmente saludable.", */
             audioUrl: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1JQdiQpxxkNw7XQLWGeUkP?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
           },
         ],
